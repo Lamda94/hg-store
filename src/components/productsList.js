863c2818f4e08d90cde0b75c6a251c3001dbfae4
Luis Alberto Mendez de Avila <38790016+Lamda94@users.noreply.github.com>
2021-06-17 18:59:02 -0500

    data.json

--- src/components/productsList.js
+++ src/components/productsList.js
@@ -2,12 +2,13 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import Product from './Product'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
     const [products, setProducts] = useState([]);
     console.log(products); 
     useEffect(() => {           
         setTimeout(() => {
-            axios('/data/data.json')
+            axios.get('/data/data.json')
             .then((res) => {
                 setProducts(res.data);
             })
@@ -16,10 +17,10 @@ export default () => {
     }, []);
         
     return ( 
-        <div className="row row-cols-1 row-cols-md-3 g-4" key="0">
+        <div className="row row-cols-1 row-cols-md-3 g-4" >
             {    
                 products.map(product=>{
-                    return <Product name={product.name} descr={product.description} src={product.images} availability={product.availability}></Product>
+                    return <Product idkey={product.id} name={product.name} descr={product.description} src={product.images} availability={product.availability}></Product>
                 })
             }
         </div>

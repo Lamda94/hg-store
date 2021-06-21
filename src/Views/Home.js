6facc2f61973626690ac04720555d64c0607a362
Lamda94 <38790016+Lamda94@users.noreply.github.com>
2021-06-21 15:00:13 -0500

    home page

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -8,6 +8,10 @@ export default function Home() {
             <div className="d-flex justify-content-center">
                 <CategoriesList/>
             </div>
+            <div className="row bg-secondary mt-5 mb-5 text-light p-4">
+                <h1 className="text-center fw-bold lh-1 inline-block p-3">¿Why choose us?</h1>
+                <p className="fs-4 lh-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod at animi adipisci praesentium. Accusamus accusantium dolorem doloribus, autem nam, alias assumenda voluptates quaerat ut, corrupti illo pariatur natus placeat minima.</p>
+            </div>
         </>
     )
 }

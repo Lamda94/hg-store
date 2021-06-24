b75953e8c1ecc28e0d618ec79560d05939d35d27
Lamda94 <38790016+Lamda94@users.noreply.github.com>
2021-06-24 09:14:44 -0500

    .

--- src/components/productsList.js
+++ src/components/productsList.js
@@ -1,27 +1,28 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import Product from './Product'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Product from './Product';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {           
-        setTimeout(() => {
-            axios.get('/data/data.json')
-            .then((res) => {
-                setProducts(res.data);
-            })
-            .catch(console.log);
-        }, 2000);
-    }, []);
-        
-    return ( 
-        <div>
-            <div className="row row-cols-1 row-cols-md-3 g-4 p-3" >
-                {    
-                    products.map((product)=><Product key={product.id} data={product} />)
-                }
-            </div>
-        </div>        
-    )           
-}
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get('/data/data.json')
+        .then((res) => {
+          setProducts(res.data);
+        })
+        .catch(console.log);
+    }, 2000);
+  }, []);
+
+  return (
+    <div>
+      <div className="row row-cols-1 row-cols-md-3 g-4 p-3">
+        {products.map((product) => (
+          <Product key={product.id} data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};

40bf7b6bc801ce534702354d8f9452d43049002d
Lamda94 <38790016+Lamda94@users.noreply.github.com>
2021-07-23 15:03:50 -0500

    about

--- src/Views/About.js
+++ src/Views/About.js
@@ -13,25 +13,46 @@ export default function Store() {
             / <span className="text-muted">Nosotros</span>
           </p>
         </div>
-        <div className="d-flex align-items-center flex-column" style={{"height":"100vh"}}>
+        <div
+          className="d-flex align-items-center flex-column mb-5"
+          style={{ height: '100vh' }}
+        >
+          <div className="col-7 mb-5 mt-3">
+            <h1 className="text-center mb-4">Quienes Somos</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+              eveniet beatae. Qui quasi magni assumenda distinctio itaque
+              maxime, porro doloremque alias provident cum maiores reiciendis
+              nostrum. Sit amet similique consequuntur. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Voluptatum hic delectus earum
+              consequuntur sed minima, architecto blanditiis nisi ad eligendi
+              possimus labore animi minus, cum non amet modi recusandae dolor!
+            </p>
+          </div>
           <div className="col-7 mt-5 mb-5">
-              <h1 className="text-center mb-4">Quienes Somos</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, eveniet beatae. Qui quasi magni assumenda distinctio itaque maxime, porro doloremque alias provident cum maiores reiciendis nostrum. Sit amet similique consequuntur.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum hic delectus earum consequuntur sed minima, architecto blanditiis nisi ad eligendi possimus labore animi minus, cum non amet modi recusandae dolor! 
-              </p>
-          </div>  
-          <div className="col-7 mt-5 mb-5">
-              <h1 className="text-center mb-4">Mision y Vision</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, eveniet beatae. Qui quasi magni assumenda distinctio itaque maxime, porro doloremque alias provident cum maiores reiciendis nostrum. Sit amet similique consequuntur.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum hic delectus earum consequuntur sed minima, architecto blanditiis nisi ad eligendi possimus labore animi minus, cum non amet modi recusandae dolor! 
-              </p>
-          </div>  
-          <div className="col-7 mt-5 mb-5">
-              <h1 className="text-center mb-4">Historia</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, eveniet beatae. Qui quasi magni assumenda distinctio itaque maxime, porro doloremque alias provident cum maiores reiciendis nostrum. Sit amet similique consequuntur.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum hic delectus earum consequuntur sed minima, architecto blanditiis nisi ad eligendi possimus labore animi minus, cum non amet modi recusandae dolor! 
-              </p>
-          </div>  
+            <h1 className="text-center mb-4">Misi&oacute;n y Visi&oacute;n</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+              eveniet beatae. Qui quasi magni assumenda distinctio itaque
+              maxime, porro doloremque alias provident cum maiores reiciendis
+              nostrum. Sit amet similique consequuntur. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Voluptatum hic delectus earum
+              consequuntur sed minima, architecto blanditiis nisi ad eligendi
+              possimus labore animi minus, cum non amet modi recusandae dolor!
+            </p>
+          </div>
+          <div className="col-7 mt-5 mb-5 pb-5">
+            <h1 className="text-center mb-4">Historia</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+              eveniet beatae. Qui quasi magni assumenda distinctio itaque
+              maxime, porro doloremque alias provident cum maiores reiciendis
+              nostrum. Sit amet similique consequuntur. Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Voluptatum hic delectus earum
+              consequuntur sed minima, architecto blanditiis nisi ad eligendi
+              possimus labore animi minus, cum non amet modi recusandae dolor!
+            </p>
+          </div>
         </div>
       </div>
     </div>
